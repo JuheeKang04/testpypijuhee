2f9d7dd3c09b2664cb9cc8751c132a661d9e9e3f
JuheeKang04 <treeinblu@gmail.com>
2020-09-21 16:38:44 +0900

    set install_requires, download_url

--- setup.py
+++ setup.py
@@ -9,11 +9,10 @@ setup(
   author = 'Kang Juhee',
   author_email = 'treeinblu@gmail.com',
   url = 'https://github.com/JuheeKang04/testpypijuhee',
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/JuheeKang04/testpypijuhee/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['TEST'],
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+  install_requires=[
+          'kss',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
